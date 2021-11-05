91fece042072028857b595863c3b1be35647bb97
Wangdi Tenzing <wangdi457@gmail.com>
2021-11-05 14:19:45 -0400

    changed the baseurl again

--- client/src/requestMethod.js
+++ client/src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://salty-chamber-29544.herokuapp.com/";
+const BASE_URL = "https://salty-chamber-29544.herokuapp.com/api/";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNmQ4ODg5ZDY1MzVkMWYwNGRkMzRkMiIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2MzUxMTMyNTIsImV4cCI6MTYzNTM3MjQ1Mn0.rsxlc8taQd_nbvpaB2qSaezBgPaw5zuunMQcAOlYyT4 ";
 
